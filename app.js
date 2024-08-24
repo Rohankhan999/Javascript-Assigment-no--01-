61b8f34e9cb0b6725a93ad3dc0d2015cc73c3bfc
Rohankhan999 <rohankhan08453@gmail.com>
2024-08-25 00:08:50 +0500

    Js assigment

---
+++ app.js
@@ -0,0 +1,66 @@
+// Assigment no 01 (Alerts)//
+    
+//   Question no - 01//
+
+// alert("Thanks For Visit Our Website");//
+
+//   Question no - 02//
+
+// alert("Error! PLease Enter a Valid Password..");
+
+//   Question no - 03//
+
+// alert("Welcome to JS Land.. \n Happy Coding!");
+
+//   Question no - 04//
+
+// alert("Welcome to JS Land..");
+// alert(" Happy Coding!");
+
+
+//   Question no - 05//
+
+// alert("Hello... I Can Run JS Through My Web Browser's Console");
+
+
+// Assigment No - 01 (Variable);
+
+//   Question no - 01//
+
+// var  Username ;  
+
+//   Question no - 02//
+
+// var  myName = "Rohan"; 
+
+//   Question no - 03//
+
+// var  messege = "Hello World...";
+// alert(messege); 
+
+//   Question no - 04//
+
+// var  Name = "Ali" ;
+// alert(Name);
+
+// var  Age = "17 Years Old" ;
+// alert(Age);
+
+// var  passion = "Certified web devloper" ;
+// alert(passion);
+
+
+//   Question no - 05//
+
+// var Pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(Pizza);
+
+//   Question no - 06//
+
+// var greet = " my email adress is";
+// var email = " Saylani@email.com";
+// var result = greet.concat(email);
+//  alert(result);      
+
+
+
